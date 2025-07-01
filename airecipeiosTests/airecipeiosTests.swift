f2ad2323711597335418b54ac8f033fc96d1a159
Paul <ddanss@gmail.com>
2025-07-01 16:05:35 -0700

    Initial Commit

---
+++ airecipeiosTests/airecipeiosTests.swift
@@ -0,0 +1,17 @@
+//
+//  airecipeiosTests.swift
+//  airecipeiosTests
+//
+//  Created by Paul on 7/1/25.
+//
+
+import Testing
+@testable import airecipeios
+
+struct airecipeiosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
