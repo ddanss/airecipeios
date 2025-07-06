f69eeebdf9df1a45c190863e51e5e5a81a81bae6
Paul <ddanss@gmail.com>
2025-07-05 19:13:04 -0700

    connect to fbai and save.

---
+++ airecipeios/Combine/FetchRecipe.swift
@@ -0,0 +1,43 @@
+//
+//  FetchRecipe.swift
+//  airecipeios
+//
+//  Created by Paul on 7/5/25.
+//
+
+import Foundation
+import SwiftData
+import FirebaseAI
+
+class FetchRecipe {
+    @MainActor
+    static func fetchRecipe(prompt: String, modelContext: ModelContext) async {
+        let model = FirebaseAI.firebaseAI(backend: .googleAI()).generativeModel(modelName: "gemini-2.5-flash", generationConfig: GenerationConfig(
+            responseMIMEType: "application/json",
+            responseSchema: Schema.object(properties: [
+                "title": .string(),
+                "ingredients": Schema.array(items: Schema.object(properties: [
+                    "name": .string(),
+                    "quantity": .string(),
+                    "unit": .string()
+                ])),
+                "instructions": .string()
+            ])
+        ))
+        Task { @MainActor in
+            do {
+                let response = try await model.generateContent(prompt)
+                let jsonData = Data(response.text!.utf8)
+                let recipeDictionary = try JSONSerialization.jsonObject(with: jsonData) as! [String: Any]
+                let recipe: Recipe = Recipe(
+                    title: recipeDictionary["title"] as? String ?? "Title",
+                    ingredients: recipeDictionary["ingredients"] as? String ?? "NA",
+                    instructions: recipeDictionary["instructions"] as? String ?? "NA"
+                )
+                modelContext.insert(recipe)
+            } catch {
+                print("ERROR")
+            }
+        }
+    }
+}
--- airecipeios/Views/RecipesView.swift
+++ airecipeios/Views/RecipesView.swift
@@ -13,32 +13,30 @@ struct RecipesView: View {
     
     @Environment(\.modelContext) private var modelContext
     @Query var recipes: [Recipe]
+    @Query var ingredients: [Ingredient]
     @State private var searchText: String = ""
     
     var body: some View {
         VStack(spacing: 0) {
             HStack {
-                TextField("Search Recipes", text: $searchText)
+                TextField("I'm feeling like cooking something...", text: $searchText)
                     .padding(12)
                     .background(Color(.systemGray6))
                     .cornerRadius(8)
                     .layoutPriority(1)
                 Button(action: {
-                    let model = FirebaseAI.firebaseAI(backend: .googleAI()).generativeModel(modelName: "gemini-2.5-flash", generationConfig: GenerationConfig(
-                        responseMIMEType: "application/json",
-                        responseSchema: Schema.object(properties: [
-                            "title": .string(),
-                            "ingredients": Schema.array(items: Schema.object(properties: [
-                                "name": .string(),
-                                "quantity": .string(),
-                                "unit": .string()
-                            ])),
-                            "instructions": .string()
-                        ])
-                    ))
-                    let prompt = "Give me one cooking recipe."
+                    var prompt = "Give me one cooking recipe."
+                    if !searchText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty {
+                        prompt += " I want something \(searchText)."
+                    }
+                    let checkedIngredients = ingredients.filter { !$0.checked }
+                    if !checkedIngredients.isEmpty {
+                        prompt += " The only ingredients I have are \(ingredients.map((\.name)).joined(separator: ", "))."
+                    }
                     
-                    
+                    Task {
+                        await FetchRecipe.fetchRecipe(prompt: prompt, modelContext: modelContext)
+                    }
                 }) {
                     Text("Search")
                         .padding(.horizontal)
@@ -46,11 +44,12 @@ struct RecipesView: View {
                         .background(Color.accentColor)
                         .foregroundColor(.white)
                         .cornerRadius(8)
+                        .frame(minWidth: 100)
                 }
             }
             .padding(.horizontal)
             .padding(.top)
-            .frame(maxHeight: .infinity, alignment: .top)
+            .frame(maxHeight: 100, alignment: .top)
             .layoutPriority(1)
             if recipes.isEmpty {
                 
